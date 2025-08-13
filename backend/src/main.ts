66e9b3e77bc06ad73b4a28576ac37ade525cea38
Doyen Simango <1602758@students.wits.ac.za>
2025-08-13 15:23:33 +0200

    merge with frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
    const app = await NestFactory.create(AppModule);
    
 app.enableCors({
-  origin: 'http://localhost:4200',
+  origin: ['https://studynester.pages.dev', 'http://localhost:4200'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 });
