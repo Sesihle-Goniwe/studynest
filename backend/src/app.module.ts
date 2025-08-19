8c3564a0cc5dd13c61610d4af415294c4c269c90
WakkyCodes <wakky47@hotmail.com>
2025-08-19 21:47:06 +0200

    feat : Added fully working file upload functionality

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,11 +4,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SupabaseModule } from './supabase/supabase.module';
 import { StudentsModule } from './students/students.module';
+import { FilesModule } from './files/files.module';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    SupabaseModule, StudentsModule],
+    SupabaseModule, 
+    StudentsModule, 
+    FilesModule,
+    MulterModule.register({
+      dest: './uploads', // Optional: specify a destination for temporary files
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
