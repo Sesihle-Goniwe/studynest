fdcba74a76aab183de0efc1d56a285374192553c
WakkyCodes <wakky47@hotmail.com>
2025-08-31 21:03:24 +0200

    update : added imports for backend deployment

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,6 +6,11 @@ import { SupabaseModule } from './supabase/supabase.module';
 import { StudentsModule } from './students/students.module';
 import { FilesModule } from './files/files.module';
 import { MulterModule } from '@nestjs/platform-express';
+import { GroupsModule } from './groups/groups.module';
+import { SessionsModule } from './sessions/sessions.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { MailerModule } from './mailer/mailer.module';
+import { ProgressModule } from './progress/progress.module';
 
 @Module({
   imports: [
