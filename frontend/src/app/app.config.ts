f0a2343bfe404acc50e4c59786098f3d34b709fa
WakkyCodes <wakky47@hotmail.com>
2025-09-01 15:18:23 +0200

    fix : added markdown import in app config files

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideMarkdown } from 'ngx-markdown'; 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideHttpClient(withFetch())
+    provideRouter(routes), provideHttpClient(withFetch()),
+    provideMarkdown()
   ]
 };
